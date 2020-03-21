8defe15e493b572a6eb3dbe68209e18e99de73f7
Yury Z <jobenergo@gmail.com>
2020-03-21 17:33:21 -0600

    msg1

---
+++ src/test/java/training/Task_3.java
@@ -0,0 +1,36 @@
+package training;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Task_3 {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void loginAsAdmin() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.cssSelector("input[name='username']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button[name='login']")).click();
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
