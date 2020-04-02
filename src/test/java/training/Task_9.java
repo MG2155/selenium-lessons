5ccb7a8d78d1c5de2b9fd04d914152950eb6e7fc
Yury Z <jobenergo@gmail.com>
2020-04-01 19:36:21 -0600

    04/01/2020 Comleted Task_9

--- src/test/java/training/Task_9.java
+++ src/test/java/training/Task_9.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,31 +18,82 @@ public class Task_9 {
     private WebDriver driver;
 
     @BeforeTest
-    public void start() throws InterruptedException {
+    public void start() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.cssSelector("input[name='username']")).sendKeys("admin");
         driver.findElement(By.cssSelector("input[name='password']")).sendKeys("admin");
         driver.findElement(By.cssSelector("button[name='login']")).click();
-        Thread.sleep(1500);
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
     }
 
-    @Test(priority = 1)
-    public void mainBtn() throws InterruptedException {
-        List<WebElement> allMainBtns = driver.findElements(By.id("app-"));
-        for (int i = 0; i < allMainBtns.size(); i++) {
-            List<WebElement> items = driver.findElements(By.id("app-"));
-            items.get(i).click();
-            Thread.sleep(250);
-            Assert.assertNotNull(driver.findElement(By.cssSelector("h1[style*='margin']")).getText());
-            List<WebElement> other = driver.findElements(By.cssSelector("li[id*='doc']"));
-            for (int j = 0; j < other.size(); j++) {
-                List<WebElement> others = driver.findElements(By.cssSelector("li[id*='doc']"));
-                others.get(j).click();
-                Thread.sleep(250);
-                Assert.assertNotNull(driver.findElement(By.cssSelector("h1[style*='margin']")).getText());
+    @Test
+    public void task91a() {
+        List<WebElement> allRows = driver.findElements(By.xpath("//tr/td[5]/a"));
+        String[] arrayOfNames = new String[allRows.size()];
+        for (int i = 0; i < allRows.size(); i++) {
+            String str = allRows.get(i).getAttribute("text");
+            arrayOfNames[i] = str;
+        }
+        Arrays.sort(arrayOfNames);
+        for (int j = 0; j < allRows.size(); j++) {
+            Assert.assertEquals(allRows.get(j).getAttribute("text"), arrayOfNames[j]);
+        }
+    }
+
+    @Test
+    public void task91b() {
+        List<WebElement> allZones = driver.findElements(By.xpath("//tbody/tr/td[6]"));
+        for (int i = 0; i < allZones.size(); i++) {
+            driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+            List<WebElement> allZonesNew = driver.findElements(By.xpath("//tbody/tr/td[6]"));
+            String numberOfZones = allZonesNew.get(i).getAttribute("innerText");
+            int result = Integer.parseInt(numberOfZones);
+            if (result != 0) {
+                List<WebElement> countryList = driver.findElements(By.xpath("//tr/td[5]/a"));
+                countryList.get(i).click();
+                List<WebElement> zonesListone = driver.findElements(By.xpath("//table[2]/tbody/tr/td[3]/input[@type=\"hidden\"]"));
+                String[] stringArrayOfZonesSorted = new String[zonesListone.size()];
+                String[] stringArrayOfZonesNotSorted = new String[zonesListone.size()];
+                for (int n = 0; n < zonesListone.size(); n++) {
+                    List<WebElement> zonesList = driver.findElements(By.xpath("//table[2]/tbody/tr/td[3]/input[@type=\"hidden\"]"));
+                    String zone = zonesList.get(n).getAttribute("value");
+                    stringArrayOfZonesSorted[n] = zone;
+                    stringArrayOfZonesNotSorted[n] = zone;
+                }
+                Arrays.sort(stringArrayOfZonesSorted);
+                for (int m = 0; m < zonesListone.size(); m++) {
+                    Assert.assertEquals(stringArrayOfZonesSorted[m], stringArrayOfZonesNotSorted[m]);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void task92() {
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+        List<WebElement> countries = driver.findElements(By.xpath("//tbody/tr/td[3]/a"));
+        for (int i = 0; i < countries.size(); i++) {
+            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+            List<WebElement> countriesNew = driver.findElements(By.xpath("//tbody/tr/td[3]/a"));
+            countriesNew.get(i).click();
+
+            List<WebElement> items = driver.findElements(By.xpath("//tbody/tr/td[3]/select/option[@selected=\"selected\"]"));
+            String[] arrayForSelectedValuesSorted = new String[items.size()];
+            String[] arrayForSelectedValuesNOTSorted = new String[items.size()];
+
+            for (int j = 0; j < items.size(); j++) {
+                List<WebElement> itemsNew = driver.findElements(By.xpath("//tbody/tr/td[3]/select/option[@selected=\"selected\"]"));
+                String str = itemsNew.get(j).getAttribute("textContent");
+                arrayForSelectedValuesSorted[j] = str;
+                arrayForSelectedValuesNOTSorted[j] = str;
+
+            }
+            Arrays.sort(arrayForSelectedValuesSorted);
+            for (int m = 0; m < items.size(); m++) {
+                Assert.assertEquals(arrayForSelectedValuesSorted[m], arrayForSelectedValuesNOTSorted[m]);
             }
         }
     }
